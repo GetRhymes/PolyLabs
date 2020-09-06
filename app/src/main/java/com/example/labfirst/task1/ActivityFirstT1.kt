14a1235e0f306901fdc7951a609ff48905d397b4
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2020-09-07 01:09:49 +0300

    part of first lab

---
+++ app/src/main/java/com/example/labfirst/task1/ActivityFirstT1.kt
@@ -0,0 +1,12 @@
+package com.example.labfirst.task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.labfirst.R
+
+class ActivityFirstT1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_t1)
+    }
+}
